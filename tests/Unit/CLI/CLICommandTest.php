4cd340d38af12c3149053c47b6eaf4c32d803904
dhanyled <dhanyled@users.noreply.github.com>
2026-08-30 09:41:24 +0700

    test: expand unit test coverage for FontController and CLICommand

---
+++ tests/Unit/CLI/CLICommandTest.php
@@ -0,0 +1,31 @@
+﻿<?php
+declare(strict_types=1);
+
+namespace WPSpeedCore\Tests\Unit\CLI;
+
+use PHPUnit\Framework\TestCase;
+use WP_Mock;
+use WPSpeedCore\CLI\CLICommand;
+
+class CLICommandTest extends TestCase {
+
+    public function setUp(): void {
+        WP_Mock::setUp();
+    }
+
+    public function tearDown(): void {
+        WP_Mock::tearDown();
+    }
+
+    public function test_purge_executes_action_and_logs_success(): void {
+        if (!class_exists('WP_CLI')) {
+            eval('class WP_CLI { public static function success($m){} public static function error($m){} public static function line($m){} }');
+        }
+
+        WP_Mock::expectAction('wpsc_purge_all');
+
+        $cli = new CLICommand();
+        $cli->purge([], []);
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/Optimization/FontControllerTest.php
@@ -0,0 +1,34 @@
+﻿<?php
+declare(strict_types=1);
+
+namespace WPSpeedCore\Tests\Unit\Optimization;
+
+use PHPUnit\Framework\TestCase;
+use WP_Mock;
+use WPSpeedCore\Optimization\FontController;
+
+class FontControllerTest extends TestCase {
+
+    public function setUp(): void {
+        WP_Mock::setUp();
+    }
+
+    public function tearDown(): void {
+        WP_Mock::tearDown();
+    }
+
+    public function test_ensure_display_swap_injects_display_swap(): void {
+        WP_Mock::userFunction('get_option')
+            ->with('wpsc_settings', [])
+            ->andReturn([
+                'font' => ['swap_google_fonts' => true],
+            ]);
+
+        $controller = new FontController();
+
+        $html = '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">';
+        $output = $controller->ensure_display_swap($html);
+
+        $this->assertStringContainsString('&amp;display=swap', $output);
+    }
+}
